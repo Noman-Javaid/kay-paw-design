7874f1d144c5ac2499af5726aeac747909c56a25
Ali Shahbaz <m.alishahbaz01@gmail.com>
2025-02-25 20:03:03 +0500

    Setup FolderFairy

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,60 @@
 import React, { useState } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import Sidebar from './Pages/dashboard/Sidebar';
-import Dashboard from './Pages/dashboard/Dashboard';
-import PostListing from './Pages/dashboard/PostListing';
-import MyProperties from './Pages/dashboard/MyProperties';
-import SavedHomes from './Pages/dashboard/SavedHomes';
-import ManageTours from './Pages/dashboard/ManageTours';
-import Profile from './Pages/dashboard/Profile';
-import LandingPage from './Pages/landingPage/LandingPage';
-import MapPage from './Pages/MapPage/MapPage';
-import PropertyDetail from './Pages/PropertyDetailPage/PropertyDetail';
-import Customers from './Pages/Customers/Customers';
+
+//   KAYPAW
+// import Sidebar from './Pages/KaypawDesign/dashboard/Sidebar';
+// import Dashboard from './Pages/KaypawDesign/dashboard/Dashboard';
+// import PostListing from './Pages/KaypawDesign/dashboard/PostListing';
+// import MyProperties from './Pages/KaypawDesign/dashboard/MyProperties';
+// import SavedHomes from './Pages/KaypawDesign/dashboard/SavedHomes';
+// import ManageTours from './Pages/KaypawDesign/dashboard/ManageTours';
+// import Profile from './Pages/KaypawDesign/dashboard/Profile';
+// import LandingPage from './Pages/KaypawDesign/landingPage/LandingPage';
+// import MapPage from './Pages/KaypawDesign/MapPage/MapPage';
+// import PropertyDetail from './Pages/KaypawDesign/PropertyDetailPage/PropertyDetail';
+// import Customers from './Pages/KaypawDesign/Customers/Customers';
+
+// FOLDER FAIRY
+
+
+
+
+
 
 const App = () => {
-  const [page, setPage] = useState('dashboard');
+  // KAYPAW
+  // const [page, setPage] = useState('dashboard');
 
   return (
-    <>
-      <Customers />
-      {/* <PropertyDetail /> */}
-      {/* <MapPage /> */}
-      {/* <LandingPage /> */}
-    </>
+    //KAYPAW
+
+    // <div>
+    //   <Customers />
+    //   {/* <PropertyDetail /> */}
+    //   {/* <MapPage /> */}
+    //   {/* <LandingPage /> */}
+    // </div>
     // <div className="flex min-h-screen">
     //   <div>
-    //     <Sidebar setPage={setPage} activePage={page} />
-    //   </div>
-    //   <div className="flex-grow p-4">
-    //     {page === 'dashboard' && <Dashboard />}
-    //     {page === 'postListing' && <PostListing />}
-    //     {page === 'myProperties' && <MyProperties />}
-    //     {page === 'savedHomes' && <SavedHomes />}
-    //     {page === 'manageTours' && <ManageTours />}
-    //     {page === 'profile' && <Profile />}
-    //   </div>
+    //       <Sidebar setPage={setPage} activePage={page} />
+    //     </div>
+    //     <div className="flex-grow p-4">
+    //       {page === 'dashboard' && <Dashboard />}
+    //       {page === 'postListing' && <PostListing />}
+    //       {page === 'myProperties' && <MyProperties />}
+    //       {page === 'savedHomes' && <SavedHomes />}
+    //       {page === 'manageTours' && <ManageTours />}
+    //       {page === 'profile' && <Profile />}
+    //     </div>
     // </div>
+
+
+    // FOLDER FAIRY
+
+    <>
+
+
+    </>
   );
 };
 
